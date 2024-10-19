1780610f17a4ce76c4fcdc432928ab9ec827b5a1
Kiki Ardhana <kikiardhana88@gmail.com>
2024-10-20 03:05:12 +0700

    perbaiki style

--- component/navbar.php
+++ component/navbar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div class="container-fluid">
         <a class="navbar-brand ml-5" href="#">Brand</a>
@@ -25,32 +24,4 @@
             </ul>
         </div>
     </div>
-=======
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand ml-5" href="#">Brand</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end " id="navbarNav">
-            <ul class="navbar-nav mr-2">
-                <li class="nav-item">
-                    <a class="nav-link" href="#home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#reward">Reward</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#menu">Menu</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
->>>>>>> a23f66bc57ae35c8d0ab02f52ee0966e49e75a24
 </nav>
